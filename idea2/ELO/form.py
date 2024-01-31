449bd5ac757e4de69d1e3949a99885ac0958a41f
Marc Folch <marcfolchpomares@gmail.comm>
2024-01-31 18:59:34 -0500

    New changes

--- idea2/ELO/form.py
+++ idea2/ELO/form.py
@@ -1,52 +1,53 @@
-import streamlit as st
-st.write("Hello")
+# IMPORT LIBRARIES
 
+import streamlit as st
 import pandas as pd
 from pymongo import MongoClient
 
-# Replace these values with your MongoDB Atlas connection string and database/collection information
+
+
+# OPEN OBJECTIVES TABLE
+
 connection_string = "mongodb+srv://marcfolchpomares:AstonMartin1@mycluster.e19nlo1.mongodb.net/?retryWrites=true&w=majority"
-
-# Connect to MongoDB Atlas
 client = MongoClient(connection_string)
-
-# Access a specific database (replace 'your_database' with your actual database name)
 db = client.ELO_gym
-
-# Access a specific collection within the database (replace 'your_collection' with your actual collection name)
 collection = db.objectives
 
-# Query all documents from the collection
 cursor = collection.find({})
 data = list(cursor)
-
-# Create a DataFrame from the retrieved data
 df = pd.DataFrame(data)
 
-bodypart = df["Bodypart"].unique()
 
-# Create a select box
+
+# FORM AND TRANSFORMATION
+
+bodypart = df["Bodypart"].unique()
 selected_bodypart = st.selectbox('Bodypart:', bodypart)
 
 exercise = df[df["Bodypart"] == selected_bodypart]["Exercise"].unique()
-
-# Create a select box
 selected_exercise = st.selectbox('Exercise:', exercise)
 
 selected_weight = st.number_input("Weight:", step=0.1)
 
 selected_repetitions = st.number_input("Repetitions:", step=1)
 
+converter = 0.453592
+multiplier = 1.899371069
 
-# Access a specific collection within the database (replace 'your_collection' with your actual collection name)
-collection = db.data
+def calculator (weight, repetitions):
+    return ((weight/5)*16)+(repetitions-(3*(weight/5)))
+
+elo = ((calculator(selected_weight*converter, selected_repetitions) - df[df["Exercise"]==selected_exercise]["Actual Score"].item()) / (df[df["Exercise"]==selected_exercise]["Goal Score"].item() - df[df["Exercise"]==selected_exercise]["Actual Score"].item()) * 2400)
+
+
+
+# OPEN DATA TABLE TO CREATE A NEW ROW WHEN FORM SUBMITTED
 
 from datetime import datetime
+collection = db.data
 
-# Get current date and time
 current_datetime = datetime.now()
-
-new_row_data = {"Date":current_datetime, "Bodypart":selected_bodypart, "Exercise":selected_exercise, "Weight":selected_weight, "Repetitions":selected_repetitions}
+new_row_data = {"Date":current_datetime, "Bodypart":selected_bodypart, "Exercise":selected_exercise, "Weight":selected_weight, "Repetitions":selected_repetitions, "ELO":elo}
 
 if st.button("Insert"):
     collection.insert_one(new_row_data)
---
+++ idea2/ELO/pages/01ELO.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from pymongo import MongoClient
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+
+connection_string = "mongodb+srv://marcfolchpomares:AstonMartin1@mycluster.e19nlo1.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string)
+db = client.ELO_gym
+collection = db.data
+
+cursor = collection.find({})
+data = list(cursor)
+
+# Create a DataFrame from the retrieved data
+data = pd.DataFrame(data)
+
+bodypart = data["Bodypart"].unique()
+
+selected_bodypart = st.selectbox("Bodypart:",bodypart)
+
+elo = round(data[data["Bodypart"]==selected_bodypart]["ELO"].mean(), 0)
+
+st.markdown(f'<p class="btc_text">ELO<br></p><p class="price_details">{elo}</p>', unsafe_allow_html = True)
