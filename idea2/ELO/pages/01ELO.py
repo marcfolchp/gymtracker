fa42952407ae856a12c0fa7328bcb15bbe00c4fa
Marc Folch <marcfolchpomares@gmail.comm>
2024-01-31 19:29:25 -0500

    new

--- idea2/ELO/pages/01ELO.py
+++ idea2/ELO/pages/01ELO.py
@@ -6,21 +6,8 @@ import shutil
 
 import streamlit.components.v1 as components
 
-# HACK This works when we've installed streamlit with pip/pipenv, so the
-# permissions during install are the same as the running process
-STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
-# We create a videos directory within the streamlit static asset directory
-# and we write output files to it
-VIDEOS_PATH = (STREAMLIT_STATIC_PATH / "styles")
-if not VIDEOS_PATH.is_dir():
-    VIDEOS_PATH.mkdir()
-
-wildlife_video = VIDEOS_PATH / "style.css"
-if not wildlife_video.exists():
-    shutil.copy("style.css", wildlife_video)  # For newer Python.
-
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+# with open('style.css') as f:
+#     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 connection_string = "mongodb+srv://marcfolchpomares:AstonMartin1@mycluster.e19nlo1.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
@@ -39,4 +26,27 @@ selected_bodypart = st.selectbox("Bodypart:",bodypart)
 
 elo = round(data[data["Bodypart"]==selected_bodypart]["ELO"].mean(), 0)
 
-st.markdown(f'<p class="btc_text">ELO<br></p><p class="price_details">{elo}</p>', unsafe_allow_html = True)
+st.markdown(f'<style>'
+             f'@import url("https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap");'
+             f'.price_details {{'
+             f'    font-size: 80px; '
+             f'    font-family: "Space Grotesk", sans-serif;'
+             f'    color: #f6f6f6;'
+             f'    font-weight: 900;'
+             f'    text-align: center;'
+             f'    line-height: 1;'
+             f'    margin-bottom: 100px;'
+             f'}}'
+             f'.btc_text {{'
+             f'    font-size: 20px;'
+             f'    font-family: "Space Grotesk", sans-serif;'
+             f'    color: #a1a1a1;'
+             f'    font-weight: bold;'
+             f'    text-align: center;'
+             f'    line-height: 0.2;'
+             f'    padding-top: 10px;'
+             f'}}'
+             f'</style>'
+             f'<p class="btc_text">ELO<br></p>'
+             f'<p class="price_details">{elo}</p>', 
+             unsafe_allow_html=True)
